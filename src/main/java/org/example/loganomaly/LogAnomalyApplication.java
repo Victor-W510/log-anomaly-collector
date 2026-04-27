efca5b820d04400f3dab21c444a4655bf6b2bca0
Victor W <victor.westerberg@yh.nackademin.se>
2026-04-27 23:25:10 +0200

    first commmit

---
+++ src/main/java/org/example/loganomaly/LogAnomalyApplication.java
@@ -0,0 +1,12 @@
+package org.example.loganomaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnomalyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogAnomalyApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/loganomaly/LogAnomalyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.loganomaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnomalyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
