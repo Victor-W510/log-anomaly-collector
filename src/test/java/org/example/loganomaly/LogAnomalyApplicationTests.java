8c2b8409fe08abfb97a542e97e02b8bd5708efae
Victor W <victor.westerberg@yh.nackademin.se>
2026-05-02 22:59:53 +0200

    feat: Add Tests

--- src/test/java/org/example/loganomaly/LogAnomalyApplicationTests.java
+++ src/test/java/org/example/loganomaly/LogAnomalyApplicationTests.java
@@ -1,13 +1,70 @@
 package org.example.loganomaly;
 
+import org.example.loganomaly.model.Level;
+import org.example.loganomaly.model.LogEntity;
+import org.example.loganomaly.repository.Repository;
+import org.example.loganomaly.service.LogGeneratorService;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 @SpringBootTest
 class LogAnomalyApplicationTests {
 
+    @MockitoBean
+    private Repository repository;
+
+    @Autowired
+    private LogGeneratorService service;
+
     @Test
-    void contextLoads() {
+    void generateGoodLogs() throws InterruptedException {
+        service.generateGoodLogs();
+        verify(repository, times(1)).save(any(LogEntity.class));
     }
 
+    @Test
+    void generateGoodLogs_shouldHaveInfoLevel() throws InterruptedException {
+        ArgumentCaptor<LogEntity> captor = ArgumentCaptor.forClass(LogEntity.class);
+
+        service.generateGoodLogs();
+
+        verify(repository).save(captor.capture());
+
+        LogEntity saved = captor.getValue();
+        assertEquals(Level.INFO, saved.getLevel());
+    }
+
+    @Test
+    void generateBadLogs_shouldSaveLog() throws InterruptedException {
+        service.generateBadLogs();
+
+        verify(repository, atLeastOnce()).save(any(LogEntity.class));
+    }
+
+    @Test
+    void ddos_shouldGenerateMultipleLogs() {
+        service.ddos();
+
+        verify(repository, atLeast(10)).save(any(LogEntity.class));
+    }
+
+    @Test
+    void generateGoodLogs_shouldSetResponseTime() throws InterruptedException {
+        ArgumentCaptor<LogEntity> captor = ArgumentCaptor.forClass(LogEntity.class);
+
+        service.generateGoodLogs();
+
+        verify(repository).save(captor.capture());
+
+        LogEntity log = captor.getValue();
+
+        assertNotNull(log.getResponseTime());
+        assertTrue(log.getResponseTime() >= 0);
+    }
 }
